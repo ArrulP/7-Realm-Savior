f152a6be5b9296acc76dd49fe2a9bb159df0abed
Arrul Patrao <thealexmasonofficial@gmail.com>
2026-06-13 17:03:31 +0530

    Started inventory system
    Started inventory system

---
+++ Assets/Scripts/Items/DefaultObject.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[CreateAssetMenu(fileName="New item", menuName="Inventory System/Item")]
+public class DefaultObject : ItemObject
+{
+    public int attack;
+    public int spattack;
+    public int hp;
+    public int haste;
+    public int speed;
+
+}
---
+++ Assets/Scripts/Items/ItemObject.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public abstract class ItemObject : ScriptableObject
+{
+    public GameObject prefab;
+    public string Description;
+}
